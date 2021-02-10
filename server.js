575f9cd6df6a154a52c755aa4329f93e68fa1bc8
AdeThorMiwa <bendamyth@gmail.com>
2021-02-10 08:05:23 +0100

    fixing login and signup button disable error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ connectDb();
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`app is running on port ${PORT}`));
 
-process.on("SIGTERM", () => {
+process.on("SIGTERM", () => { 
   console.log("SIGTERM RECIEVED");
   server.close(() => {
     console.log("shutting down");
